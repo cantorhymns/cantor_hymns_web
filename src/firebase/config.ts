093788f4825fb2ce848f8317a9ab5edb5cbc8d45
Andrew Ekladious <andrewekladious@gmail.com>
2025-10-14 03:02:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-127742305-c9528",
+  "appId": "1:631193516314:web:7e38f2ef132365857ed4a3",
+  "apiKey": "AIzaSyCW1NCoI8NPnVfXYBWEWmbf5mkWVEV7x_M",
+  "authDomain": "studio-127742305-c9528.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "631193516314"
+};
