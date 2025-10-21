d0ca12a894bd451702f16c72e02e514832f462d9
Andrew Ekladious <andrewekladious@gmail.com>
2025-10-21 20:55:58 +0000

    Can you review my Firebase database and let me know if anything needs to

--- src/lib/seed-data.ts
+++ src/lib/seed-data.ts
@@ -40,56 +40,45 @@ export const hymns = [
         "id": "kata-ni-khoros",
         "name": "Kata Ni Khoros",
         "genreId": "holy-week"
-    },
-    {
-        "id": "phai-etafenf",
-        "name": "Phai Etafenf",
-        "genreId": "holy-week"
     }
 ];
 
 export const recordings = [
     {
         "hymnId": "tai-shouri",
-        "cantor": "Cantor Bola",
+        "cantor": "CantorBola",
         "audioUrl": "CantorBola/CantorBola_MournfulTaishouri.mp3",
         "marks": [0, 15, 30, 45, 60]
     },
     {
         "hymnId": "omonogenees",
-        "cantor": "Cantor Bola",
+        "cantor": "CantorBola",
         "audioUrl": "CantorBola/CantorBola_Omonogenees.mp3",
         "marks": [0, 15, 30, 45, 60]
     },
     {
         "hymnId": "tarh",
-        "cantor": "Cantor Gad",
+        "cantor": "CantorGad",
         "audioUrl": "CantorGad/CantorGad_Tar7.mp3",
         "marks": [0, 15, 30, 45, 60]
     },
     {
         "hymnId": "kata-ni-khoros",
-        "cantor": "Cantor Ibrahim",
+        "cantor": "CantorIbrahim",
         "audioUrl": "CantorIbrahim/CantorIbrahim_KataNiKhoros7egab.mp3",
         "marks": [0, 15, 30, 45, 60]
     },
     {
         "hymnId": "tarh",
-        "cantor": "Cantor Ibrahim",
+        "cantor": "CantorIbrahim",
         "audioUrl": "CantorIbrahim/CantorIbrahim_Tar7.mp3",
         "marks": [0, 15, 30, 45, 60]
     },
     {
         "hymnId": "tai-shouri",
-        "cantor": "Cantor Tharwat",
+        "cantor": "CantorTharwat",
         "audioUrl": "CantorTharwat/CantorTharwat_MournfulTaishouri.mp3",
         "marks": [0, 18, 36, 54, 72, 90, 108]
-    },
-    {
-        "hymnId": "phai-etafenf",
-        "cantor": "Cantor Tharwat",
-        "audioUrl": "CantorTharwat/CantorTharwat_Phaietafenf.mp3",
-        "marks": [0, 15, 30, 45, 60]
     },
     {
         "hymnId": "kata-ni-khoros",
