b2a516338c94f239ef420b2bd14b1783b915156b
Andrew Ekladious <andrewekladious@gmail.com>
2025-12-15 05:23:22 +0000

    It does not work. This is the link I see:
    https://firebasestorage.googl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-127742305-c9528",
   "appId": "1:631193516314:web:7e38f2ef132365857ed4a3",
-  "storageBucket": "studio-127742305-c9528.appspot.com",
+  "storageBucket": "studio-127742305-c9528.firebasestorage.app",
   "apiKey": "AIzaSyCW1NCoI8NPnVfXYBWEWmbf5mkWVEV7x_M",
   "authDomain": "studio-127742305-c9528.firebaseapp.com",
   "measurementId": "",
